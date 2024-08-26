5225c993d38acd187a90be31d8f4a771fa60c789
SimonHofer11 <simon.hofer@uni-ulm.de>
2024-08-26 16:06:58 +0200

    strategy_prompt und zugehörige Funktionen

--- scripts/ai_mod.R
+++ scripts/ai_mod.R
@@ -74,8 +74,9 @@ new_player_df = function(i, n_rounds) {
     plan = rep("", n_rounds),
     #Hier die bisherigen Pläne einfügn
     hist_text = rep("", n_rounds),
-    plan_pompt = rep("", n_rounds),
-    q_prompt = rep("", n_rounds)
+    plan_prompt = rep("", n_rounds),
+    q_prompt = rep("", n_rounds),
+    strategy_prompt = rep("", n_rounds)
   )
 
 }
@@ -106,6 +107,7 @@ game_play_next_round = function(game) {
     game = player_make_history_text(game, i)
 
     game = player_make_plan_prompt(game, i)
+    game = player_make_strategy_prompt(game,i)
     game = player_run_plan_prompt(game, i)
 
     # TO DO
@@ -122,19 +124,23 @@ game_play_next_round = function(game) {
 player_run_plan_prompt = function(game, i, attempt=1, max_attempts = 10) {
   restore.point("player_run_plan_prompt")
   t = game$cur_round
+  player_make_strategy_prompt(game,i)
   df = game$player_dfs[[i]]
-
-  prompt = df$plan_pompt[[t]]
-
-  if (IS_ON_GHA) {
+  prompt = df$strategy_prompt[t]
+  
+  if(IS_ON_GHA){
+    #TO-DO Hier noch überprüfen ob geht
     res = run_text_prompt(prompt, max_tries)
-  } else {
+    #TO-DO: zudem format so machen dass in Variable nur text steht und in res$text gespeichert wird
+  } else{
     res = list(
       ok = TRUE,
       #hier dann prompt übergeben
       text = "-- I am the plan --"
     )
+
   }
+
   #Wenn es fehlermeldung gibt/Antwort nicht passt -->nochmals probieren
   if (!res$ok) {
     if (attempt > max_attempts) {
@@ -152,7 +158,7 @@ player_run_plan_prompt = function(game, i, attempt=1, max_attempts = 10) {
 
 }
 
-
+###Kann man  deleten--< durch strategy_prompt ersetzt
 player_make_plan_prompt = function(game, i) {
   restore.point("player_run_plan_prompt")
   df = game$player_dfs[[i]]
@@ -162,7 +168,7 @@ player_make_plan_prompt = function(game, i) {
   #Make Strategy prompt übergeben
   prompt = paste0("-- PLAN PROMPT FOR i = ",i," t = ", t)
 
-  df$plan_pompt[t] = prompt
+  df$plan_prompt[t] = prompt
   game$player_dfs[[i]] = df
   game
 }
@@ -223,7 +229,7 @@ player_make_history_text = function(game, i) {
   # Build history text from past prices, q and profits for the player
 
   #hist_text = "-- I WILL BE THE HISTORY TEXT--" # TO DO
-  hist_text = "To help you, we have listed the quantities you have placed so far, the resulting market prices and your profits:\n\n"
+  hist_text = "Furthermore, to help you, we have listed the quantities you have placed so far, the resulting market prices and your profits:\n\n"
   
   for (round in 1:(t-1)) {
     # Add the results of each round to the history text
@@ -242,10 +248,52 @@ player_make_history_text = function(game, i) {
 
 }
 
-make_strategy_prompt = function(player, round, history_text, prev_strategy) {
-  # returns prompt as text
-  # strategy as text for player, in round,
+#Noch nicht im Projekt integriert
+player_make_strategy_prompt = function(game,i){
+  # Update game with the current round's history text
+  game <- player_make_history_text(game,i)
+  
+  # Get the history text for the current round
+  df <- game$player_dfs[[i]]
+  t <- game$cur_round
+  hist_text <- df$hist_text[t]
+  
+  # Define the template with the history text included
+  prompt_text <- paste0(
+    "Let us do a small simulation experiment. I understand that you're a language model and not a decision-maker, but let's simulate what a participant in this experiment might do.\n",
+    "In this experiment you repeatedly make decisions with the goal to maximize your profit. How much profit you make depends on your decisions and on the decisions of another participant. The other participant receives the same instructions.\n",
+    "You will stay anonymous for us and for the other randomly chosen participant you contact during the experiment.\n",
+    "In this experiment you represent a firm that, like the other firm, produces and sells one and the same product on a market. You will be constantly matched with the same participant. Costs of production are 1$ per unit (this holds for the other firm as well).\n",
+    "In each round, firms will always have to make one decision, namely what quantity they wish to produce. The following important rule holds: The larger the total quantity of all firms, the smaller the price in the market. Moreover, the price will be zero from a certain amount of total output upwards.\n",
+    "Your profit per unit of output will be the difference between the market price and the unit cost of 1$. Note that you will make a loss if the market price is below the unit costs.\n",
+    "Your profit per round is, thus, equal to the profit per unit multiplied by the number of Units you sell.\n",
+    "In each round the output decisions of both firms will be registered, the corresponding price will be determined and the profits will be computed.\n",
+    "We play a total of 25 rounds. In each round you can write down a strategic plan and choose a quantity between 0 and 100 in 0.01 steps.\n\n",
+    "To help you for the plan and simulate what quantity a participant might choose, we have provided you with a profit calculator, which allows you to calculate your own profit based on your quantity and the quantity of the other participant in the market. The function (exemplarily written in the R programming language) is as follows:\n",
+    "profit_calculator <- function(q_i, q_others) {\n",
+    "  Q <- sum(q_others) + q_i  # sum(q_others): sum of the quantity of other market members, q_i is the own quantity\n",
+    "  p <- max(100 - Q, 0)    # price based on the total quantity\n",
+    "  revenue <- p * q_i\n",
+    "  costs <- q_i\n",
+    "  profit <- revenue - costs\n",
+    "  return(profit)\n",
+    "}\n\n",
+    hist_text, "\n\n",  
+    "Based on these information, I would like you to write down your plans for what quantity strategy to test in the next round. Be detailed and precise but keep things succinct, while considering the provided rules.\n",
+    "Simply write the plan for the next round in the field provided.\n",
+    "Plan in the next round: <fill in plan here>\n"
+  )
+  df$strategy_prompt[t] = prompt_text
+  
+  game$player_dfs[[i]] = df
+  
+  
+  game
+  
+}
 
+make_strategy_prompt = function(game,i) {
+  
 }
 
 make_q_prompt = function(player, round, history_text, strategy) {
