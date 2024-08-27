479a5a3d3a3be390c7aac14f1ccc8300383730f5
SimonHofer11 <simon.hofer@uni-ulm.de>
2024-08-27 15:02:26 +0200

    script demand and profit calculation to main

---
+++ scripts/script_demand_profit.R
@@ -0,0 +1,24 @@
+#Demand function
+price_function <- function(Q) {
+  return(max(100 - Q, 0))
+}
+
+
+# Profit Calculator
+profit_calculator <- function(q_i, q_others) {
+  Q <- sum(q_others) + q_i  # aggregated market quantity
+  p <- price_function(Q)    
+  revenue <- p * q_i         
+  costs <- q_i 
+  profit <- revenue - costs  
+  return(profit)
+}
+
+# One Period calculation
+simulate_period <- function(q_i, q_others) {
+  profit <- profit_calculator(q_i, q_others)
+  Q_total <- sum(q_others) + q_i
+  price = price_function(Q_total)
+  Q_other = sum(q_others)
+  return(list(profit = profit, price = price, Q_other = Q_other, Q_total = Q_total))
+}
