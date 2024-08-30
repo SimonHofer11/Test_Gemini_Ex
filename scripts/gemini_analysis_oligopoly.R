d2819818ee80f0e84db517e8697388e4d5e4d4b3
SimonHofer11 <simon.hofer@uni-ulm.de>
2024-08-30 10:26:37 +0200

    Update gemini_analysis_oligopoly.R

--- scripts/gemini_analysis_oligopoly.R
+++ scripts/gemini_analysis_oligopoly.R
@@ -29,49 +29,58 @@ perform_analysis = function() {
   config_df = load_prompt_configs()
   
   
-  game = new_game(n_players=3, n_rounds = 10)
+  game = new_game(n_players=2, n_rounds = 10)
   traceback()
   game = run_game(game, debug_mode = FALSE)
   game$ok
   game$err_msg
   
   options(warn=2)
+
+  out_file = paste0(outdir, "/", "oligopoly_experiment",cur_time,".Rds")
+  saveRDS(res, out_file)
+  
+
+cat("\n\nFINISHED\n\n")
+  
+}  
     
     
-  #Das kann dann hier weg.  
-  prompt_files = list.files("prompts", glob2rx("*.txt"),full.names = TRUE)
-  file = first(prompt_files)
-
-  for (file in prompt_files) {
-    prompt_start_time = as.numeric(Sys.time())
-
-    prompt_name = tools::file_path_sans_ext(basename(file))
-    cat("\n\n****", prompt_name, "***\n")
-    res = analyse_prompt_file(file, config_df=config_df, api_key = API_KEY)
-    #Hier wird relevante Antwort gespeichert, mit der ich dann file +1 ändere.
-    #Irgendwie durch: prompt_files[file +1] = funktion_x(prompt_file+1, res$candidates[[x]][[xx]])
-    #diese funktion muss erst einen check geben (oder durch andere funktion), ob Sturktur passnt, ansonsten muss loop nochmals neu durchlaufen
-    
-    out_file = paste0(outdir, "/", prompt_name,".Rds")
-    saveRDS(res, out_file)
-
-    cur_time = as.numeric(Sys.time())
-
-    # Check total run time
-    if (cur_time - start_time > MAX_RUNTIME_SEC) {
-      cat("\nStop because total runtime exceeded ", MAX_RUNTIME_SEC, " seconds.\n")
-      return()
-    }
-    wait_sec = MIN_SEC_PER_PROMPT-(cur_time - prompt_start_time)
-    if (wait_sec > 0) {
-      cat("\nWait for ", round(wait_sec), "seconds...")
-      Sys.sleep(wait_sec)
-    }
-
-  }
-
-  cat("\n\nFINISHED\n\n")
-}
+#   #Das kann dann hier weg.  
+#   prompt_files = list.files("prompts", glob2rx("*.txt"),full.names = TRUE)
+#   file = first(prompt_files)
+# 
+#   for (file in prompt_files) {
+#     prompt_start_time = as.numeric(Sys.time())
+# 
+#     prompt_name = tools::file_path_sans_ext(basename(file))
+#     cat("\n\n****", prompt_name, "***\n")
+#     res = analyse_prompt_file(file, config_df=config_df, api_key = API_KEY)
+#     #Hier wird relevante Antwort gespeichert, mit der ich dann file +1 ändere.
+#     #Irgendwie durch: prompt_files[file +1] = funktion_x(prompt_file+1, res$candidates[[x]][[xx]])
+#     #diese funktion muss erst einen check geben (oder durch andere funktion), ob Sturktur passnt, ansonsten muss loop nochmals neu durchlaufen
+#     
+#     out_file = paste0(outdir, "/", prompt_name,".Rds")
+#     saveRDS(res, out_file)
+# 
+#     cur_time = as.numeric(Sys.time())
+# 
+#     # Check total run time
+#     if (cur_time - start_time > MAX_RUNTIME_SEC) {
+#       cat("\nStop because total runtime exceeded ", MAX_RUNTIME_SEC, " seconds.\n")
+#       return()
+#     }
+#     wait_sec = MIN_SEC_PER_PROMPT-(cur_time - prompt_start_time)
+#     if (wait_sec > 0) {
+#       cat("\nWait for ", round(wait_sec), "seconds...")
+#       Sys.sleep(wait_sec)
+#     }
+# 
+#   }
+#   
+# 
+#   cat("\n\nFINISHED\n\n")
+# }
 
 
 analyse_prompt_file = function(file, config_df,  api_key,verbose=TRUE, add_prompt=TRUE){
@@ -95,6 +104,37 @@ analyse_prompt_file = function(file, config_df,  api_key,verbose=TRUE, add_promp
   }
   res
 }
+
+
+analyse_prompt_oligopoly = function(prompt, config_df,  api_key,verbose=TRUE, add_prompt=TRUE){
+  if (verbose) {
+    cat(paste0("\n\nANALYSE ", file,"\n\n"))
+  }
+  prompt = paste0(readLines(file,warn = FALSE), collapse="\n")
+
+  #prompt_name = tools::file_path_sans_ext(basename(file))
+  
+
+  res = run_gemini(prompt, api_key, json_mode=config$json_mode, model=config$model, temperature = 1)
+  
+  
+  #res$prompt_name = prompt_name
+  #if (isTRUE(config$add_prompt)) {
+  #  res$prompt = prompt
+  #}
+  res
+}
+
+
+
+
+
+
+
+
+
+
+
 
 get_prompt_config = function(file, config_df) {
   def_config = config_df[config_df$prompt_type=="_default",]
