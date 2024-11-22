48d3c8b458f1f70ad78e051bfe549825a94eebcb
SimonHofer11 <73963211+SimonHofer11@users.noreply.github.com>
2024-11-22 08:53:02 +0100

    Update run_oligopoly.R

--- scripts_oligopoly/run_oligopoly.R
+++ scripts_oligopoly/run_oligopoly.R
@@ -1,5 +1,5 @@
 #Define the number of players (if you select several, the game will be repeated several times)
-N_PLAYERS = c(5,5) #c(2)
+N_PLAYERS = c(5) #c(2)
 PLAYER_TYPE = c("PRO","FLASH","PRO","PRO","PRO")
 N_ROUNDS = 25
 
